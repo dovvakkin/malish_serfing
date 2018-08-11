197c54614d8188f819af59e2ed3f1e045f463441
dovvakkin <dovvakkin@gmail.com>
2018-08-11 11:14:28 +0300

    add scroll fix

--- Main.py
+++ Main.py
@@ -20,12 +20,17 @@ def hitroClick(self):
     link_presser.move_to_element_with_offset(self, 2, 2).click().perform()
 
 
+def double_scroll(self):
+    scroll_action = ActionChains(self)
+    scroll_action.key_down(Keys.UP).key_up(Keys.UP).key_down(Keys.UP).key_up(Keys.UP).perform()
+
+
 def scan_tasks():  # parse target for all tasks
-    sleep(3)
+    sleep(3) # wait for page loaded
     task_list = driver.find_elements_by_xpath("//div[@class='join-group']")
     scroll = driver.find_element_by_tag_name("html")
     scroll.send_keys(Keys.END)
-    sleep(3)
+    sleep(1)
 
     for task in reversed(task_list):
         if str(task.find_element_by_xpath(".//span").text).startswith("Вступить в сообщество"):
@@ -37,17 +42,17 @@ def scan_tasks():  # parse target for all tasks
                     link.hitroClick()
                     break
                 except selenium.common.exceptions.MoveTargetOutOfBoundsException:
-                    scroll.send_keys(Keys.UP)
+                    driver.double_scroll()
 
             driver.switch_to.window(driver.window_handles[-1])
             vk_handler.join_group(driver)
-            sleep(3)
+            sleep(1)
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
 
             link = task.find_element_by_xpath(".//a[2]")
             link.hitroClick()
-            sleep(3)
+            sleep(2)
 
             #TODO:
 
@@ -60,7 +65,7 @@ def scan_tasks():  # parse target for all tasks
                     link.hitroClick()
                     break
                 except selenium.common.exceptions.MoveTargetOutOfBoundsException:
-                    scroll.send_keys(Keys.UP)
+                    driver.double_scroll()
 
             driver.switch_to.window(driver.window_handles[-1])
             vk_handler.add_to_friends(driver)
@@ -76,6 +81,7 @@ def scan_tasks():  # parse target for all tasks
 
 # webdriverTUNING
 selenium.webdriver.firefox.webelement.FirefoxWebElement.hitroClick = hitroClick
+selenium.webdriver.firefox.webelement.FirefoxWebElement.double_scroll = double_scroll
 # end of webdriverTUNING
 
 counter = 0
@@ -85,8 +91,8 @@ while True:
             general_login()
             scan_tasks()
             logging.info('выход на охоту: '+str(datetime.now().strftime('%Y_%m_%d_%H:%M:%S')))
-        except Exception:
-            error = str('!!!выход провален: ' + str(datetime.now().strftime('%Y_%m_%d_%H:%M:%S'))+'\n'+str(Exception))
+        except Exception as e:
+            error = str('!!!выход провален: ' + str(datetime.now().strftime('%Y_%m_%d_%H:%M:%S'))+'\n'+e)
             logging.error(error)
     counter = (counter + 1) % 14 # burnaya noch' simulation
     sleep(60 * random.uniform(61, 75))  # чтоб типа как человек
